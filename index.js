eb52fcd71d1ee24c28da2f1ec5575a674cb3a491
ditikrushna <diti.geek@gmail.com>
2021-07-20 18:40:42 +0530

    simple node app with test

---
+++ index.js
@@ -0,0 +1,12 @@
+//importing node framework
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (request, response) {
+    response.status(200).send("Hello World");
+})
+//listen to port 3000 by default
+app.listen(process.env.PORT || 3000);
+
+module.exports = app;
